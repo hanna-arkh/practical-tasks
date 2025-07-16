a316c5e87303607c2d4cf0ab268dbbdcd0c3be3a
AnnArkhip <annarkhipenkoaa@gmail.com>
2025-07-16 18:08:07 +0300

    added eslint and prettier

--- orientation/components/Orientation.js
+++ orientation/components/Orientation.js
@@ -1,37 +1,21 @@
-import {
-  useWindowDimensions,
-  View,
-  Text,
-  StyleSheet,
-  SafeAreaView,
-} from "react-native";
+import { useWindowDimensions, Text, StyleSheet, SafeAreaView } from 'react-native'
 
 export default function Orientation() {
-  const { width, height } = useWindowDimensions();
-  const isPortrait = height >= width;
+  const { width, height } = useWindowDimensions()
+  const isPortrait = height >= width
   return (
-    <SafeAreaView
-      style={[{ flexDirection: isPortrait ? "column" : "row" }, styles.text]}
-    >
-      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>
-        Lorem ipsum dolor sit amet
-      </Text>
-      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>
-        Nunc malesuada sagittis feugiat
-      </Text>
-      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>
-        Morbi ipsum tellus, dictum ac lorem vel
-      </Text>
-      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>
-        blandit porttitor mauris
-      </Text>
+    <SafeAreaView style={[{ flexDirection: isPortrait ? 'column' : 'row' }, styles.text]}>
+      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>Lorem ipsum dolor sit amet</Text>
+      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>Nunc malesuada sagittis feugiat</Text>
+      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>Morbi ipsum tellus, dictum ac lorem vel</Text>
+      <Text style={{ fontSize: isPortrait ? 8 : 18 }}>blandit porttitor mauris</Text>
       <Text style={{ fontSize: isPortrait ? 8 : 18 }}>sagittis feugiat</Text>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   text: {
-    alignItems: "center",
+    alignItems: 'center',
   },
-});
+})
