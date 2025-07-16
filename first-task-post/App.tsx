1a78dff3263d5162b368ec3a04c7c3661353504d
AnnArkhip <annarkhipenkoaa@gmail.com>
2025-07-16 17:27:10 +0300

    switched app.tsx to an arrow function

--- first-task-post/App.tsx
+++ first-task-post/App.tsx
@@ -2,7 +2,7 @@ import { View } from 'react-native'
 import { Post } from './components/Post'
 import { styles } from './styles/styles'
 
-export default function App() {
+export const App = () => {
   return (
     <View style={styles.containerApp}>
       <Post />
