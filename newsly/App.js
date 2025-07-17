b75cb6c6dd617096b3d85a3fc2b9673b1191d798
AnnArkhip <annarkhipenkoaa@gmail.com>
2025-07-17 10:49:53 +0300

    deleted unnecessary comments and imports

--- newsly/App.js
+++ newsly/App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import BottomTab from "./components/BottomTab";
+import { NavigationContainer } from '@react-navigation/native'
+import BottomTab from './components/BottomTab'
 
 export default function App() {
   return (
     <NavigationContainer>
       <BottomTab />
     </NavigationContainer>
-  );
+  )
 }
