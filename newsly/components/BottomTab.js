60dc6188167b864f3386e89443f4a68dd6fba058
AnnArkhip <annarkhipenkoaa@gmail.com>
2025-07-17 15:00:04 +0300

    added constants for screens and icons

--- newsly/components/BottomTab.js
+++ newsly/components/BottomTab.js
@@ -1,27 +1,28 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Feed from "./Feed";
-import Saved from "./Saved";
-import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Feed from './Feed'
+import Saved from './Saved'
+import { Ionicons } from '@expo/vector-icons'
+import { iconNames, screenNames } from '../constants/constants'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 export default function BottomTab() {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
         tabBarIcon: ({ color, size, focused }) => {
-          let iconName;
-          if (route.name === "Feed") {
-            iconName = focused ? "list" : "list-outline";
-          } else if (route.name === "Saved") {
-            iconName = focused ? "bookmark" : "bookmark-outline";
+          let iconName
+          if (route.name === screenNames.feed) {
+            iconName = focused ? iconNames.listFocused : iconNames.listUnfocused
+          } else if (route.name === screenNames.saved) {
+            iconName = focused ? iconNames.bookmarkFocused : iconNames.bookmarkUnfocused
           }
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return <Ionicons name={iconName} size={size} color={color} />
         },
       })}
     >
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Saved" component={Saved} />
+      <Tab.Screen name={screenNames.feed} component={Feed} />
+      <Tab.Screen name={screenNames.saved} component={Saved} />
     </Tab.Navigator>
-  );
+  )
 }
---
+++ newsly/constants/constants.ts
@@ -0,0 +1,11 @@
+export const iconNames = {
+  listFocused: 'list',
+  listUnfocused: 'list-outline',
+  bookmarkFocused: 'bookmark',
+  bookmarkUnfocused: 'bookmark-outline',
+}
+
+export const screenNames = {
+  feed: 'Feed',
+  saved: 'Saved',
+}
