ae7c68e1c385b431f8eb2cdd08051af2859b2005
hanna-arkh <hanna.arkhipenka@innowise.com>
2025-07-22 14:38:06 +0300

    module created

---
+++ expo-settings/example/App.tsx
@@ -0,0 +1,10 @@
+import * as Settings from "expo-settings";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Theme: {Settings.getTheme()}</Text>
+    </View>
+  );
+}
