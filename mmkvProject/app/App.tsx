d41ada9e276bdb55fc40bbd2fdc5a5b8a8bd854f
hanna-arkh <hanna.arkhipenka@innowise.com>
2025-07-29 00:10:42 +0300

    added global mmkv storage

--- mmkvProject/app/App.tsx
+++ mmkvProject/app/App.tsx
@@ -2,9 +2,11 @@ import { View } from 'react-native'
 import { MmkvTheme } from '../components/MmkvTheme'
 import { AsyncTheme } from '../components/AsyncTheme'
 import { styles } from '../styles/styles'
+import { storage } from '../src/storage/storageMMKV'
 
 export default function App() {
-  const s = styles()
+  const initialTheme = storage.getString('theme') ?? 'light'
+  const s = styles(initialTheme)
   return (
     <View style={s.containerApp}>
       <MmkvTheme />
